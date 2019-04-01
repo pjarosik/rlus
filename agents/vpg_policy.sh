5ec6be81f5648d89c2c65f205363baa2a07d32b1
Piotr Jarosik <jarosik.piotr@gmail.com>
2019-04-01 11:15:32 +0200

    Connected spinup.VPG with UsPhantomEnv.

---
+++ agents/vpg_policy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [ -z "${PROJECT_PATH}" ]; then
+    echo "PROJECT_PATH is uneset/empty, set it to path, where the project is placed. "
+    exit 1
+fi
+export PYTHONPATH=${PROJECT_PATH}:$PYTHONPATH
+python3 vpg_policy.py "$@"
